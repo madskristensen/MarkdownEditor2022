38d5004fd3b6b4e045832269a590a3d1067f9303
Sam Harwell <Sam.Harwell@microsoft.com>
2022-01-06 13:58:36 -0800

    Add placeholder integration test

---
+++ MarkdownEditor2022.IntegrationTests/AbstractIntegrationTest.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.VisualStudio.Extensibility.Testing;
+using Xunit;
+
+namespace MarkdownEditor2022.IntegrationTests
+{
+    [IdeSettings(MinVersion = VisualStudioVersion.VS2022, MaxVersion = VisualStudioVersion.VS2022)]
+    public abstract class AbstractIntegrationTest : AbstractIdeIntegrationTest
+    {
+    }
+}
---
+++ MarkdownEditor2022.IntegrationTests/InitialTest.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace MarkdownEditor2022.IntegrationTests
+{
+    public class InitialTest : AbstractIntegrationTest
+    {
+        [IdeFact]
+        public void TestOpenVisualStudio()
+        {
+        }
+    }
+}
