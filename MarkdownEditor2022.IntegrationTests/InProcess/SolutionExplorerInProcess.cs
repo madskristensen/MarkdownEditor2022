8740213072e9f7d7d963aef1480c7ec6c9de9bcf
Sam Harwell <Sam.Harwell@microsoft.com>
2022-01-06 15:26:50 -0800

    Add a test to open and close a .editorconfig file

---
+++ MarkdownEditor2022.IntegrationTests/InProcess/SolutionExplorerInProcess.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace Microsoft.VisualStudio.Extensibility.Testing
+{
+    internal partial class SolutionExplorerInProcess
+    {
+        public async Task OpenFileAsync(string filePath, CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            VsShellUtilities.OpenDocument(ServiceProvider.GlobalProvider, filePath, VSConstants.LOGVIEWID.Code_guid, out _, out _, out _, out IVsTextView view);
+
+            // Reliably set focus using NavigateToLineAndColumn
+            IVsTextManager textManager = await GetRequiredGlobalServiceAsync<SVsTextManager, IVsTextManager>(cancellationToken);
+            ErrorHandler.ThrowOnFailure(view.GetBuffer(out IVsTextLines textLines));
+            ErrorHandler.ThrowOnFailure(view.GetCaretPos(out int line, out int column));
+            ErrorHandler.ThrowOnFailure(textManager.NavigateToLineAndColumn(textLines, VSConstants.LOGVIEWID.Code_guid, line, column, line, column));
+        }
+
+        public async Task CloseFileAsync(string filePath, bool saveFile, CancellationToken cancellationToken)
+        {
+            await CloseFileAsync(filePath, VSConstants.LOGVIEWID.Code_guid, saveFile, cancellationToken);
+        }
+
+        private async Task CloseFileAsync(string filePath, Guid logicalView, bool saveFile, CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            if (!VsShellUtilities.IsDocumentOpen(ServiceProvider.GlobalProvider, filePath, logicalView, out _, out _, out IVsWindowFrame? windowFrame))
+            {
+                throw new InvalidOperationException($"File '{filePath}' is not open in logical view '{logicalView}'");
+            }
+
+            __FRAMECLOSE frameClose = saveFile ? __FRAMECLOSE.FRAMECLOSE_SaveIfDirty : __FRAMECLOSE.FRAMECLOSE_NoSave;
+            ErrorHandler.ThrowOnFailure(windowFrame.CloseFrame((uint)frameClose));
+        }
+    }
+}
--- MarkdownEditor2022.IntegrationTests/InitialTest.cs
+++ MarkdownEditor2022.IntegrationTests/InitialTest.cs
@@ -1,12 +1,24 @@
-﻿using Xunit;
+﻿using System.IO;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace MarkdownEditor2022.IntegrationTests
 {
     public class InitialTest : AbstractIntegrationTest
     {
         [IdeFact]
-        public void TestOpenVisualStudio()
+        public async Task TestOpenVisualStudioAsync()
         {
+            string tempDirectory = Path.Combine(Path.GetTempPath(), "markdown-tests", Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+
+            string filePath = Path.Combine(tempDirectory, "Simple.md");
+            string contents = @"# Title
+";
+            File.WriteAllText(filePath, contents);
+
+            await TestServices.SolutionExplorer.OpenFileAsync(filePath, HangMitigatingCancellationToken);
+            await TestServices.SolutionExplorer.CloseFileAsync(filePath, saveFile: false, HangMitigatingCancellationToken);
         }
     }
 }
