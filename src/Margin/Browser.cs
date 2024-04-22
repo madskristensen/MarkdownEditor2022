a22db9f3ea0e6f66e7fcf9df8fbbb8f2a21c7190
Mads Kristensen <madskristensen@users.noreply.github.com>
2024-04-22 14:22:28 -0700

    Fixed preview padding issue [release]

--- src/Margin/Browser.cs
+++ src/Margin/Browser.cs
@@ -351,7 +351,8 @@ namespace MarkdownEditor2022
     <meta charset=""utf-8"" />
     <base href=""http://{_mappedBrowsingFileVirtualHostName}/{dirName}/"" />
     <style>
-        html, body {{margin: 0; padding: 5px 5px 10px 5px}}
+        html, body {{margin: 0; padding:0}}
+        #___markdown-content___ {{padding: 5px 5px 10px 5px;}}
         {css}
     </style>";
 
