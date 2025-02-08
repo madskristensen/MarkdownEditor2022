411e6ae745e309646707453fc2f7dc67754565fc
Mads Kristensen <madsk@microsoft.com>
2025-02-08 15:45:49 -0800

    F7 now toggles the preview pane Fixes #95

--- src/VSCommandTable.cs
+++ src/VSCommandTable.cs
@@ -36,5 +36,6 @@ namespace MarkdownEditor2022
         public const int ShowMarkdownReference = 0x1060;
         public const int Refresh = 0x1070;
         public const int GenerateTOC = 0x1080;
+        public const int TogglePreview = 0x1090;
     }
 }
